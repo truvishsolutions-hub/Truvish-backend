ed146ef687d8d444ca42addd692e0f7c153e9b05
truvishsolutions-hub <truvishsolutions@gmail.com>
2026-04-24 23:39:40 +0530

    Update DemoRequestController.java

--- src/main/java/com/truvish/truvishbackend/DemoRequest/controller/DemoRequestController.java
+++ src/main/java/com/truvish/truvishbackend/DemoRequest/controller/DemoRequestController.java
@@ -14,7 +14,6 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/api/demo-requests")
-@CrossOrigin(origins = "http://localhost:3000")
 public class DemoRequestController {
 
     private final DemoRequestService demoRequestService;
